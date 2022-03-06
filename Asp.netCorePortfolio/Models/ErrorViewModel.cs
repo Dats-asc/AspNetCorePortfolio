c58b9ed05c310460fbe3f65a0096abb36d39950c
Dats-asc <70961793+Dats-asc@users.noreply.github.com>
2022-03-06 20:32:46 +0300

    Initial commit

---
+++ Asp.netCorePortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Asp.netCorePortfolio.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
